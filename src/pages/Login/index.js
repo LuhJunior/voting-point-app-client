0cb1b161d73e8e96ae1f5ec9143340d7daea0f65
Wendell Valois <wendell.valois@gmail.com>
2019-08-02 12:44:03 -0300

    alterações na tela de login e autentificação

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,18 +8,18 @@ const Login = () => {
   return(
     <div className="container">
       <div className="login-container">
-        <h2>Login</h2>
-        <form>
-          <div>
+        <h2 className="login-title">Login</h2>
+        <form className="login-form">
+          <div className="input-container">
             <label>Matricula</label>
             <input type="text" />
           </div>
-          <div>
+          <div className="input-container">
             <label>Senha</label>
             <input type="password" />
           </div>
-          <div>
-            <button>
+          <div className="button-container">
+            <button className="button-style">
               Entrar
             </button>
           </div>
